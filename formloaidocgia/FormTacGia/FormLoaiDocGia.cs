2451a14574fc63440acc72e740aa0b2b603a755d
KhoiNV02 <khoikakaka123@gmail.com>
2022-05-24 20:20:13 +0700

    formloaidocgia

---
+++ formloaidocgia/FormTacGia/FormLoaiDocGia.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FormLoaiDocGia
+{
+    public partial class Form1 : Form
+    {
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+     
+
+        private void btnThemMoi_Click(object sender, EventArgs e)
+        {
+               
+        }
+        private void themTacGia()
+        {
+           
+        }
+     
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+          
+
+            }
+
+            
+        // Phương thức xóa tác giả
+     
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
